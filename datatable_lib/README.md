e6780d813e561313c7ca80989a0799e98aa99ccd
Vlevinski <v.levinski@gmail.com>
2019-10-13 12:39:11 +0300

    datatable class update

--- datatable_lib/README.md
+++ datatable_lib/README.md
@@ -1,2 +1,14 @@
 #
 #   MyDT class is for datatable operations
+# 
+Demo:    
+    a  b      d
+--  --  ---  ---
+ 0   1  foo  1.2
+ 1   2  bar  1.3
+ 2   3  why  1.5
+
+[3 rows x 3 columns]
+
+<class 'datatable.Frame'>
+<class 'pandas.core.frame.DataFrame'>
--- datatable_lib/datatable_.py
+++ datatable_lib/datatable_.py
@@ -1,5 +1,6 @@
 import datatable as dt
 import numpy as np
+import pandas as pd
 
 class DT():
     '''
@@ -15,21 +16,59 @@ class DT():
     def from_csv(self, file = "in.csv"):
         return dt.fread(file)
 
+    def to_csv(self, file = 'out.csv'):
+        return pd.to_csv()
+
     def names(self, table):
         return table.names
 
 out = lambda x: print(x)
 
 tb = DT()
-table = tb.from_csv()
+t = tb.from_csv(file ='datatable_lib/in.csv') # or t = tb.create
+out(t)
+out(type(t))
+t.to_csv('datatable_lib/out.csv')
 
-out(table)
-out(table.sum())
-#out(data.names)
-#out(data.shape)
-#out(data.head(1))
+#tpd = t.to_pandas()
+#out(type(tpd))
+
+
+## statistics
+#out(t.names)
+#out(t.shape)
+#out(t.head(1))
+
+## more dt methods
 #data.sum()      data.nunique()
 #data.sd()       data.max()
 #data_df.mode()  data_df.min()
 #data.nmodal()   data_df.mean()
 
+## selecting rows/columns
+#t[:,'column']
+#t[:,:]
+
+## sorting
+#t.sort['column']
+
+## deleting rows/columns
+#del t[:,'column']
+
+## groupby
+#f stands for frame proxy, and provides a simple way to refer to the Frame that we are currently operating upon.
+#for i in range(100):
+#    datatable_df[:, dt.sum(dt.f.funded_amnt), dt.by(dt.f.grade)]
+
+##filtering rows
+#datatable_df[dt.f.loan_amnt>dt.f.funded_amnt,"loan_amnt"]
+
+#Saving the Frame
+#datatable_df.to_csv('output.csv')
+
+# more links
+# https://github.com/Rdatatable/data.table/wiki
+# https://datatable.readthedocs.io/en/latest/using-datatable.html
+# https://datatable.readthedocs.io/en/latest/index.html
+# https://datatable.readthedocs.io/en/latest/using-datatable.html
+# https://www.kaggle.com/sudalairajkumar/getting-started-with-python-datatable
