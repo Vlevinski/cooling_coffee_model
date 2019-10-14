680106c779ffacbe14ef371660dedd08e97d439e
Vlevinski <v.levinski@gmail.com>
2019-10-14 08:54:47 +0300

    datatable class customization

--- datatable_lib/datatable_.py
+++ datatable_lib/datatable_.py
@@ -1,3 +1,14 @@
+#!/usr/bin/python3.7
+'''
+ Links
+ https://github.com/Rdatatable/data.table/wiki
+ https://datatable.readthedocs.io/en/latest/using-datatable.html
+ https://datatable.readthedocs.io/en/latest/index.html
+ https://datatable.readthedocs.io/en/latest/using-datatable.html
+ https://www.kaggle.com/sudalairajkumar/getting-started-with-python-datatable
+ https://github.com/Rdatatable/data.table/wiki
+'''
+
 import datatable as dt
 import numpy as np
 #import pandas as pd
@@ -5,57 +16,6 @@ import numpy as np
 class DT():
     '''
     DT class as functionality for datatable library
-    '''
-
-    def __init__(self):
-        '''
-        Deafaul in csv file
-        '''
-        self.name = 'out.csv'
-
-    def create(self, size=10):
-        '''
-        create DataTable frame
-        :param size:
-        :return:
-        '''
-        return dt.Frame(np.random.randn(size))
-
-    def read_csv(self, file = "in.csv"):
-        '''
-        read DataTable from csv file
-        :param file:  file name
-        :return:
-        '''
-        return dt.fread(file)
-
-    def write_csv(self, frame, file ='out.csv'):
-        '''
-        write DataTable to csv file
-        :param frame:  DataTable frame
-        :param file:  csv file name
-        :return:
-        '''
-        return dt.DataTable.to_csv(frame, file)
-
-    def names(self, table):
-        '''
-        list DataTable names
-        :param table:
-        :return:
-        '''
-        return table.names
-
-tb = DT()
-
-#read csv filie
-t = tb.read_csv(file ='datatable_lib/in.csv') # or t = tb.create
-
-print("DataTable shape", t.shape)
-
-#write csv
-tb.write_csv(t,'datatable_lib/out.csv')
-
 # ===========================
 ## Datatable commands :
 ## statistics
@@ -89,10 +49,55 @@ tb.write_csv(t,'datatable_lib/out.csv')
 
 #Saving the Frame
 #datatable_df.to_csv('output.csv')
+    '''
 
-# more links
-# https://github.com/Rdatatable/data.table/wiki
-# https://datatable.readthedocs.io/en/latest/using-datatable.html
-# https://datatable.readthedocs.io/en/latest/index.html
-# https://datatable.readthedocs.io/en/latest/using-datatable.html
-# https://www.kaggle.com/sudalairajkumar/getting-started-with-python-datatable
+    def __init__(self):
+        '''
+        Deafaul in csv file
+        '''
+        self.infile = "in.csv"
+        self.outfile = "out.csv"
+
+    def create(self, size=10):
+        '''
+        create DataTable frame
+        :param size:
+        :return:
+        '''
+        return dt.Frame(np.random.randn(size))
+
+    def in_csv(self, file = "in.csv"):
+        '''
+        read DataTable from csv file
+        :param file:  file name
+        :return:
+        '''
+        return dt.fread(file)
+
+    def out_csv(self, frame, file ='out.csv'):
+        '''
+        write DataTable to csv file
+        :param frame:  DataTable frame
+        :param file:  csv file name
+        :return:
+        '''
+        return dt.DataTable.to_csv(frame, file)
+
+    def names(self, table):
+        '''
+        list DataTable names
+        :param table:
+        :return:
+        '''
+        return table.names
+
+tb = DT()
+
+#read csv filie
+t = tb.in_csv(file ='datatable_lib/in.csv') # or t = tb.create
+print("DataTable shape", t.shape)
+
+#write csv
+tb.out_csv(t,'datatable_lib/out.csv')
+
+
