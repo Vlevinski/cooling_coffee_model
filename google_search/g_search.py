fe59a457ea6f95dc2f509930b87fb6681b92e0c4
Vlevinski <v.levinski@gmail.com>
2019-10-16 09:55:35 +0300

    last update

--- google_search/g_search.py
+++ google_search/g_search.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3.8
 #               @vlevinski
 #
-# Get the first 20 hits for: "Breaking Code" WordPress blog
-#       https://github.com/MarioVilas/googlesearch
-#       https://python-googlesearch.readthedocs.io/en/latest/   Doc's
+
 #from googlesearch import search
 try:
     from googlesearch import search
